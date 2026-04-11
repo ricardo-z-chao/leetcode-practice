83ecbb43553695af1e785c759d6f331e9721ebdc
ricardo <ricardo.z.chao@gmail.com>
2026-04-11 19:27:21 +0800

    build: 条件编译

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -8,6 +8,15 @@ file(
   RELATIVE ${CMAKE_SOURCE_DIR}
   "${CMAKE_SOURCE_DIR}/src/*.c")
 
+# test模块的工具函数
+if(BUILD_TESTING)
+  add_library(test_utils STATIC
+              ${CMAKE_SOURCE_DIR}/test/utils/linked_list_utils.cpp)
+  target_include_directories(
+    test_utils PUBLIC ${CMAKE_SOURCE_DIR}/test/utils/include
+                      ${CMAKE_SOURCE_DIR}/src/include)
+endif()
+
 foreach(file ${files})
   get_filename_component(FILE_NAME ${file} NAME_WE)
 
@@ -26,6 +35,13 @@ foreach(file ${files})
     target_link_libraries(
       ${FILE_NAME}_test PRIVATE ${FILE_NAME} GTest::gtest_main
                                 GTest::gmock_main)
+
+    # 条件编译，检测源文件是否包含指定的宏定义，如果包含则链接test_utils库
+    file(STRINGS ${CMAKE_SOURCE_DIR}/test/${FILE_NAME}_test.cpp CONTENT)
+    if(CONTENT MATCHES "#define INCLUDE_UTILS")
+      target_link_libraries(${FILE_NAME}_test PRIVATE test_utils)
+    endif()
+
     target_compile_features(${FILE_NAME}_test PRIVATE cxx_std_17)
     gtest_discover_tests(${FILE_NAME}_test)
   endif()
