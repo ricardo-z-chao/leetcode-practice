90ed7e8597d6f716074d67fa9725f19e1c7ec234
ricardo <ricardo.z.chao@gmail.com>
2026-04-21 15:22:44 +0800

    chore: 添加说明

--- src/sort_an_array.c
+++ src/sort_an_array.c
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 
-int* bubbleSort(int*, int, int*);
+static int* bubbleSort(int*, int, int*);
 
 /**
  * @berief leetcode 912. 排序数组
@@ -17,13 +17,14 @@ int* sortArray(int* nums, int numsSize, int* returnSize) {
 }
 
 /**
- * @berief 冒泡排序
+ * @brief 冒泡排序
+ * @note 稳定排序，时间复杂度 O(n^2)
  * @param[in] nums 输入数组
  * @param[in] numsSize 输入数组大小
  * @param[out] returnSize 输出数组大小
  * @return 排序后的数组
  */
-int* bubbleSort(int* nums, int numsSize, int* returnSize) {
+static int* bubbleSort(int* nums, int numsSize, int* returnSize) {
   for (int i = numsSize - 1; i > 0; i--) {
     for (int j = 0; j < i; j++) {
       if (nums[j] > nums[j + 1]) {
